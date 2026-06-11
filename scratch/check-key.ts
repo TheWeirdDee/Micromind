1157cc4341478bf95c9762b4c8eaa9a8cbf700a9
TheWeirdDee <divinenation1@gmail.com>
2026-06-11 13:14:06 +0100

    chore: remove temporary scratch file check-key.ts

--- scratch/check-key.ts
+++
@@ -1,6 +0,0 @@
-import { privateKeyToAccount } from 'viem/accounts';
-const key = process.argv[2] as `0x${string}`;
-if (key) {
-  const account = privateKeyToAccount(key);
-  console.log(`Address: ${account.address}`);
-}
