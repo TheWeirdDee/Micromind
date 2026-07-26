0a82e050455d4404d9e89e24c1b57cb91c0f8ff9
TheWeirdDee <divinenation1@gmail.com>
2026-07-26 13:55:22 +0100

    feat(journal): add /app/journal/new route for composing entries full-screen

---
+++ src/app/app/journal/new/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { JournalEntryEditor } from '@/components/app/JournalEntryEditor';
+
+export default function NewJournalEntryPage() {
+  return <JournalEntryEditor mode="new" />;
+}
